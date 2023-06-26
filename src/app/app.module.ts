4c9b8eba18f7d7884fcb46bc65ee86649f417db9
Christian Murillo <camurillo43@gmail.com>
2023-06-26 14:03:49 -0500

    Fin DBZ module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 // Modulos
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 // Componentes
 import { AppComponent } from './app.component';
@@ -16,8 +17,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
-
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
